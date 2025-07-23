cb71f4cef1195298c45e29a8b5a38b1aceef61a3
Roman Yerchyk <lann.wauthority@gmail.com>
2025-07-23 22:42:57 +0300

    Implement pagination for notes and enhance error handling

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watchEffect } from 'vue';
 
 const notes = ref([
   { id: 1, createdAt: '01.01.1970 00:00', updatedAt: '01.01.01.1970 00:00', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
 ]);
 
+const page = ref(1);
 const error = ref('');
 const _error = (err: any) => {
   console.log(err);
   error.value = err.toString();
 };
 
-onMounted(() => {
-  fetch('https://api.lanny.dev/notes')
-      .then(res => res.json())
-      .then(data => notes.value = data)
-      .catch(err => _error(err))
-});
+watchEffect(() => {
+  fetch(`https://api.lanny.dev/notes?limit=7&page=${page.value}`)
+    .then(res => res.json())
+    .then(data => notes.value = data)
+    .catch(err => _error(err));
+})
 
 const content = ref('');
 const addNote = () => {
@@ -37,9 +38,14 @@ const addNote = () => {
     <h2 class="font-bold text-red-500">{{ error }}</h2>
     <div class="flex max-w-xl w-full p-5 gap-2">
       <input v-model="content" class="flex-1 bg-white text-gray-700 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent px-4 py-2" placeholder="What's on your mind?" />
-      <button @click="addNote" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-2">Add</button>
+      <button @click="addNote" type="button" class="text-white bg-blue-700 hover:cursor-pointer hover:bg-blue-800 focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-2">Add</button>
     </div>
     <div class="flex flex-col h-full w-full content-center items-center">
+      <div class="flex flex-row gap-5 mb-5">
+        <p class="text-2xl hover:text-blue-600 hover:underline hover:cursor-pointer" @click="page > 1 ? page-- : void 0;">←</p>
+        <p class="text-2xl">Page: {{ page }}</p>
+        <p class="text-2xl hover:text-blue-600 hover:underline hover:cursor-pointer" @click="page++">→</p>
+      </div>
       <div class="flex flex-col w-full max-w-xl gap-3 border-b border-gray-200 pb-3 mb-3" v-for="note in notes">
         <p class="text-gray-500 font-bold text-sm">Created At: {{ note.createdAt }}</p>
         <p class="text-base text-gray-800">{{ note.content }}</p>
